667e17f27af20462edee52cc5d6adabedd67419e
Bakhtiyar Rzaliyev <brzaliev@gmail.com>
2018-05-15 18:17:44 +0500

    read cardnumber

--- piface.go
+++ piface.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/luismesas/goPi/MCP23S17"
@@ -22,21 +23,34 @@ func main() {
 	}
 
 	// buzz := pfd.OutputPins[2]
-	green := pfd.OutputPins[3]
-	red := pfd.OutputPins[4]
-	// blink(green, red)
+	// green := pfd.OutputPins[3]
+	// red := pfd.OutputPins[4]
+	zero := pfd.InputPins[0]
+	one := pfd.InputPins[1]
+
 	for {
-		if pfd.Switches[0].Value() != 0 {
-			green.AllOff()
-		} else {
-			green.AllOn()
-		}
-		if pfd.Switches[1].Value() != 0 {
-			red.AllOff()
-		} else {
-			red.AllOn()
+		switch {
+		case zero.Value() != 1:
+			readCard(pfd, 0)
+		case one.Value() != 1:
+			readCard(pfd, 1)
 		}
 	}
+
+}
+
+func readCard(pfd *piface.PiFaceDigital, firstDigit int) {
+	cardNumber := strconv.Itoa(firstDigit)
+	for i := 0; i < 35; i++ {
+		time.Sleep(time.Microsecond * 50)
+		switch {
+		case pfd.InputPins[0].Value() != 1:
+			cardNumber += "0"
+		case pfd.InputPins[1].Value() != 1:
+			cardNumber += "1"
+		}
+	}
+	fmt.Println(cardNumber)
 }
 
 func blink(green, red *MCP23S17.MCP23S17RegisterBit) {
@@ -50,5 +64,20 @@ func blink(green, red *MCP23S17.MCP23S17RegisterBit) {
 		time.Sleep(time.Second)
 		red.Toggle()
 		time.Sleep(time.Second)
+	}
+}
+
+func switchLeds(pfd *piface.PiFaceDigital) {
+	for {
+		if pfd.Switches[0].Value() != 0 {
+			pfd.OutputPins[3].AllOff()
+		} else {
+			pfd.OutputPins[3].AllOn()
+		}
+		if pfd.Switches[1].Value() != 0 {
+			pfd.OutputPins[4].AllOff()
+		} else {
+			pfd.OutputPins[4].AllOn()
+		}
 	}
 }
