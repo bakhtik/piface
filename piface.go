f45fed7b3de541574283f53f4e637cb5b4669be3
Bakhtiyar Rzaliyev <brzaliev@gmail.com>
2018-05-16 09:42:55 +0500

    millisecond

--- piface.go
+++ piface.go
@@ -38,7 +38,7 @@ func main() {
 	t := time.Now()
 	for {
 		digit := <-reader1
-		if time.Now().Sub(t) > time.Millisecond*50 {
+		if time.Now().Sub(t) > time.Millisecond*500 {
 			cardCh <- card
 			card, count = 0, 0
 		}
